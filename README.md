230678e964ea3b9459d1209c385e4bf426e1d517
张一 <2326962137@qq.com>
2020-07-23 18:17:54 +0800

    2020-07-23-18-17-33

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 初始化项目
 
 # 添加 scss 支持 node-sass
+
+# 添加 react-router-dom @types/react-router-dom
+
+# 添加 underscore lodash
---
+++ src/root/Root.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { isArray } from 'underscore';
+import _ from 'lodash';
+
+export default () => {
+  return;
+};
